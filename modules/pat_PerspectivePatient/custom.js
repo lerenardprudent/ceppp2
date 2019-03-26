b9ec17f00c253ec1f97e58c2be60f80bb9045705
lerenardprudent <daniel.margulius@gmail.com>
2019-03-25 22:47:37 -0400

    Yay.

--- modules/pat_PerspectivePatient/custom.js
+++ modules/pat_PerspectivePatient/custom.js
@@ -1,13 +1,16 @@
-console.log("HOLA BOLA");
-$('form .buttons .button:not(.primary)').remove();
-$('.sidebar').hide();
-$.cookie('sidebartoggle', 'collapsed');
-$('#buttontoggle').removeClass('button-toggle-expanded');
-$('#buttontoggle').addClass('button-toggle-collapsed');
-$('#buttontoggle').remove();
-$('#bootstrap-container').removeClass('col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 col-sm-3 col-md-2 sidebar');
-$('footer').removeClass('expandedSidebar');
-$('footer').addClass('collapsedSidebar');
-$('#bootstrap-container').removeClass('expandedSidebar');
-$('#bootstrap-container').addClass('collapsedSidebar');
-$('#quickcreatetop').remove();
+$(document).ready(function() {
+  $('form .buttons .button:not(.primary)').remove();
+  $('.sidebar').hide();
+  $.cookie('sidebartoggle', 'collapsed');
+  $('#buttontoggle').removeClass('button-toggle-expanded');
+  $('#buttontoggle').addClass('button-toggle-collapsed');
+  setTimeout(function() {
+    $('#buttontoggle').remove();
+    $('.quickcreatetop').remove();
+  }, 0);
+  $('#bootstrap-container').removeClass('col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 col-sm-3 col-md-2 sidebar');
+  $('footer').removeClass('expandedSidebar');
+  $('footer').addClass('collapsedSidebar');
+  $('#bootstrap-container').removeClass('expandedSidebar');
+  $('#bootstrap-container').addClass('collapsedSidebar');
+});
