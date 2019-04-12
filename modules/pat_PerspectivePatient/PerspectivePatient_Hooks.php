8a01c8aad081bf330e834978c9fe52905ca71c61
webexpo <daniel.margulius@gmail.com>
2019-04-12 18:54:01 -0400

    Redirecting recruters.

--- modules/pat_PerspectivePatient/PerspectivePatient_Hooks.php
+++ modules/pat_PerspectivePatient/PerspectivePatient_Hooks.php
@@ -8,13 +8,22 @@ class PerspectivePatient_Hooks {
     
     if ( $bean->module_name == "Users") {
       $roles = ACLRole::getUserRoleNames($current_user->id);
-      if ( count($roles) == 1 && $roles[0] == "Patient" ) {
-        $perspModuleName = "pat_PerspectivePatient";
-        $userId = $current_user->id;
-        $query = "SELECT * FROM ". strtolower($perspModuleName) . " WHERE assigned_user_id = \"$userId\"";
-        $recordId = $db->getOne($query);
-        if ( $recordId ) {
-          $redirectUrl = "index.php?action=ajaxui#ajaxUILoc=" . urlencode("index.php?module=$perspModuleName&return_module=$perspModuleName&action=EditView&record=$recordId");
+      if ( count($roles) == 1 ) {
+        $role = strtolower($roles[0]);
+      
+        if ( $role == "patient" ) {
+          $perspModuleName = "pat_PerspectivePatient";
+          $userId = $current_user->id;
+          $query = "SELECT * FROM ". strtolower($perspModuleName) . " WHERE assigned_user_id = \"$userId\"";
+          $recordId = $db->getOne($query);
+          if ( $recordId ) {
+            $redirectUrl = "index.php?action=ajaxui#ajaxUILoc=" . urlencode("index.php?module=$perspModuleName&return_module=$perspModuleName&action=EditView&record=$recordId");
+            SugarApplication::redirect($redirectUrl);
+            $GLOBALS['log']->debug("Redirecting: $redirectUrl");
+          }
+        } else
+        if ( $role == "recruteur" ) {
+          $redirectUrl = "index.php?module=pat_Patients&action=index";
           SugarApplication::redirect($redirectUrl);
           $GLOBALS['log']->debug("Redirecting: $redirectUrl");
         }
