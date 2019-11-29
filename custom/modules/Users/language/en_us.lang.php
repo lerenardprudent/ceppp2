3bfb248a8270994b157f8b2d12af339ed85f5400
webexpo <daniel.margulius@gmail.com>
2019-11-29 11:16:57 -0500

    Added labels.

--- custom/modules/Users/language/en_us.lang.php
+++ custom/modules/Users/language/en_us.lang.php
@@ -1,6 +1,6 @@
 <?php
 // created: 2019-11-25 17:49:41
 $mod_strings = array (
-  'LBL_DATE_LAST_LOGIN' => 'LBL_DATE_LAST_LOGIN',
-  'LBL_DATE_LAST_LOGOUT' => 'LBL_DATE_LAST_LOGOUT',
+  'LBL_DATE_LAST_LOGIN' => 'Last login',
+  'LBL_DATE_LAST_LOGOUT' => 'Last logout',
 );
---
+++ custom/modules/Users/language/fr_FR.lang.php
@@ -0,0 +1,6 @@
+<?php
+// created: 2019-11-25 17:49:41
+$mod_strings = array (
+  'LBL_DATE_LAST_LOGIN' => 'Dernière connexion',
+  'LBL_DATE_LAST_LOGOUT' => 'Dernière déconnexion',
+);
